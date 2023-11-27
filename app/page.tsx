dc6ae2db1c86a21e07b50f3e1187e4c98884f95d
daichi-iwamoto <tay1fai0y@gmail.com>
2023-11-28 00:47:31 +0900

    feat: add post list

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,35 @@
+import Link from "next/link";
+import { getPosts } from "@/libs/mdxDataFetcher";
+
 export default function Page() {
-  return <main>test</main>;
+  const contents = getPosts();
+
+  return (
+    <main>
+      <div>
+        {contents.map(
+          (
+            { title, tags, publishDate, updatedDate, description, slug },
+            index,
+          ) => (
+            <Link key={index} href={`/posts/${slug}`}>
+              <header>
+                <h3>{title}</h3>
+                <div>
+                  <small>公開日: {publishDate}</small>
+                  <small>更新日: {updatedDate}</small>
+                </div>
+              </header>
+              <div>
+                {tags.map((tag, index) => (
+                  <p key={index}>{tag}</p>
+                ))}
+              </div>
+              <p>{description}</p>
+            </Link>
+          ),
+        )}
+      </div>
+    </main>
+  );
 }
