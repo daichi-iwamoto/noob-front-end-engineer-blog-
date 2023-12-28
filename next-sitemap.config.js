4516a182e5872dee9927eb6f5d65a8401a2a6040
daichi-iwamoto <tay1fai0y@gmail.com>
2023-12-28 20:16:23 +0900

    fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl:
-    process.env.SITE_URL || "https://www.noob-front-end-engineer-blog.com/",
+  siteUrl: "https://www.noob-front-end-engineer-blog.com/",
   generateRobotsTxt: true,
 };
